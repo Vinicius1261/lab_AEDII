52c826db53137d481031eb39ef60f93b78adf243
Vinicius1261 <viniciuseduardo.santos05@gmail.com>
2025-10-08 17:12:07 -0300

    backtracking

---
+++ backtracking/scr/arq.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../func/func.h"
+
+
+int main(){
+    int x = 0, y = 0;
+    int labirinto1[10][10] = {
+    {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+    {0, 1, 1, 1, 1, 1, 1, 0, 1, 0},
+    {0, 1, 0, 0, 0, 0, 0, 0, 1, 0},
+    {0, 1, 0, 1, 1, 1, 1, 1, 1, 0},
+    {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+    {1, 1, 0, 1, 1, 1, 1, 1, 1, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 1, 1, 1, 1, 1, 1, 1, 1, 0}
+};
+
+    if (labirinto1[x][y] == 1) {
+        printf("Ponto de partida bloqueado.\n");
+        return 0;
+    }
+
+    // Tenta resolver o labirinto
+    if (resolverLabirinto(labirinto1, x, y)) {
+        printf("Labirinto resolvido:\n");
+    } else {
+        printf("Não há caminho até a saída.\n");
+    }
+
+    // Imprime o labirinto com símbolos
+    for (int i = 0; i < linha; i++) {
+        for (int j = 0; j < coluna; j++) {
+            if (labirinto1[i][j] == 0) {
+                printf(". ");
+            } else if (labirinto1[i][j] == 1) {
+                printf("# ");
+            } else if (labirinto1[i][j] == 2) {
+                printf("* ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
